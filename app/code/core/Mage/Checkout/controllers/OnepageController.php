bbfd5537e9cbb8dd85c0ff300bfa9bb056537c5e
Dru Bramlett <dru@gophilosophie.com>
2011-08-23 17:37:07 -0700

    combine shipping and billing steps

--- app/code/core/Mage/Checkout/controllers/OnepageController.php
+++ app/code/core/Mage/Checkout/controllers/OnepageController.php
@@ -293,7 +293,7 @@ class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
     public function saveBillingAction()
     {
         if ($this->_expireAjax()) {
-           // return;
+            return;
         }
         if ($this->getRequest()->isPost()) {
 //            $postData = $this->getRequest()->getPost('billing', array());
@@ -309,22 +309,22 @@ class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
             if (!isset($result['error'])) {
                 /* check quote for virtual */
                 if ($this->getOnepage()->getQuote()->isVirtual()) {
-                    // $result['goto_section'] = 'payment';
-                    // $result['update_section'] = array(
-                    //     'name' => 'payment-method',
-                    //     'html' => $this->_getPaymentMethodsHtml()
-                    // );
+                    $result['goto_section'] = 'payment';
+                    $result['update_section'] = array(
+                        'name' => 'payment-method',
+                        'html' => $this->_getPaymentMethodsHtml()
+                    );
                 } elseif (isset($data['use_for_shipping']) && $data['use_for_shipping'] == 1) {
-                    // $result['goto_section'] = 'shipping_method';
-                    // $result['update_section'] = array(
-                    //     'name' => 'shipping-method',
-                    //     'html' => $this->_getShippingMethodsHtml()
-                    // );
+                    $result['goto_section'] = 'shipping_method';
+                    $result['update_section'] = array(
+                        'name' => 'shipping-method',
+                        'html' => $this->_getShippingMethodsHtml()
+                    );
 
-                    //$result['allow_sections'] = array('shipping');
-                    //$result['duplicateBillingInfo'] = 'true';
+                    $result['allow_sections'] = array('shipping');
+                    $result['duplicateBillingInfo'] = 'true';
                 } else {
-                    //$result['goto_section'] = 'shipping';
+                    $result['goto_section'] = 'shipping';
                 }
             }
 
@@ -337,8 +337,8 @@ class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
      */
     public function saveShippingAction()
     {
-        // if ($this->_expireAjax()) {
-        //     return;
+        if ($this->_expireAjax()) {
+            return;
         }
         if ($this->getRequest()->isPost()) {
             $data = $this->getRequest()->getPost('shipping', array());
