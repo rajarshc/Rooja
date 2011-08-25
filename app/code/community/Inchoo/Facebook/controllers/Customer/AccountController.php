a25fbf15d51a330a6409d75beba89767abb7ac2d
Emerson Taymor <emerson@gophilosophie.com>
2011-08-25 11:30:06 -0700

    facebook connect redirect

--- app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
+++ app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
@@ -24,7 +24,7 @@ class Inchoo_Facebook_Customer_AccountController extends Mage_Core_Controller_Fr
 
     	if(!$this->_getSession()->validate()) {
     		$this->_getCustomerSession()->addError($this->__('Facebook connection failed.'));
-    		$this->_redirect('customer/account');
+    		$this->_redirect('/');
     		return;
     	}
     	
@@ -60,7 +60,7 @@ class Inchoo_Facebook_Customer_AccountController extends Mage_Core_Controller_Fr
 			$this->_getCustomerSession()->addSuccess(
 				$this->__('Your Facebook account has been successfully connected. Now you can fast login using Facebook Connect anytime.')
 			);
-			$this->_redirect('customer/account');
+			$this->_redirect('/');
 			return;
         }
         
@@ -122,7 +122,7 @@ class Inchoo_Facebook_Customer_AccountController extends Mage_Core_Controller_Fr
 			$this->_getCustomerSession()->addSuccess(
 				$this->__('Your Facebook account has been successfully connected. Now you can fast login using Facebook Connect anytime.')
 			);
-			$this->_redirect('customer/account/edit');
+			$this->_redirect('/');
     		return;
 		}
 		
@@ -191,7 +191,7 @@ class Inchoo_Facebook_Customer_AccountController extends Mage_Core_Controller_Fr
 			$customer->sendNewAccountEmail();
 			
 			$this->_getCustomerSession()->setCustomerAsLoggedIn($customer);
-			$this->_redirect('customer/account/edit');
+			$this->_redirect('/');
 			return;
 		
 		//else set form data and redirect to registration
