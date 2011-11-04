d1e4d82aba8ad1d70473b606e67ad00b3755b6d7
Emerson Taymor <emerson@gophilosophie.com>
2011-11-04 12:20:35 -0700

    more confirmation email testing

--- app/code/core/Mage/Newsletter/Model/Subscriber.php
+++ app/code/core/Mage/Newsletter/Model/Subscriber.php
@@ -408,7 +408,7 @@ class Mage_Newsletter_Model_Subscriber extends Mage_Core_Model_Abstract
             if ($this->getIsStatusChanged() && $status == self::STATUS_UNSUBSCRIBED) {
                 $this->sendUnsubscriptionEmail();
             } elseif ($this->getIsStatusChanged() && $status == self::STATUS_SUBSCRIBED) {
-                $this->sendConfirmationSuccessEmail();
+                //$this->sendConfirmationSuccessEmail();
             }
         }
         return $this;
