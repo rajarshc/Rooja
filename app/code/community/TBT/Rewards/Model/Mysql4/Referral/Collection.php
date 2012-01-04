e3037bb5201d526ebd39b3747a1d285d63695fae
Emerson Taymor <emerson@gophilosophie.com>
2012-01-04 14:52:20 -0500

    removed sweettooth

--- app/code/community/TBT/Rewards/Model/Mysql4/Referral/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class TBT_Rewards_Model_Mysql4_Referral_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract {
-	
-	public function _construct() {
-		$this->_init ( 'rewards/referral' );
-	}
-
-}
