2e75c3a04ee0deb7c6dad264971ebea75f6ccf20
Emerson Taymor <emerson@gophilosophie.com>
2012-01-12 19:12:32 -0500

    facebook dont allow register

--- app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
+++ app/code/community/Inchoo/Facebook/controllers/Customer/AccountController.php
@@ -204,11 +204,11 @@ class Inchoo_Facebook_Customer_AccountController extends Mage_Core_Controller_Fr
 				}
 			}
 			
-			$this->_redirect('customer/account/create');*/
-			$this->_getCustomerSession()->addError($this->__('Rooja Fashion is currently invite only. You can get on the list by clicking on the Get Invited Tab!'));
+			$this->_redirect('customer/account/create');
 			
 		}
-
+		*/
+		$this->_getCustomerSession()->addError($this->__('Rooja Fashion is currently invite only. You can get on the list by clicking on the Get Invited Tab!'));
     }
 	
 	private function _getCustomerSession()
