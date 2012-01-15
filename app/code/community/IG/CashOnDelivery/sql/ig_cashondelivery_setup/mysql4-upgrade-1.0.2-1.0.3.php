dcbade2b0437f64e52d9b990c8a9738dffbdab31
Emerson Taymor <emerson@gophilosophie.com>
2012-01-15 00:17:39 -0500

    removed IG

--- app/code/community/IG/CashOnDelivery/sql/ig_cashondelivery_setup/mysql4-upgrade-1.0.2-1.0.3.php
+++
@@ -1,7 +0,0 @@
-<?php
-$installer = $this;
-$installer->startSetup();
-$installer->run("ALTER TABLE {$this->getTable('ig_cashondelivery_local')} ADD COLUMN `fee_mode` enum('percent','absolute') NOT NULL default 'absolute'");
-$installer->run("ALTER TABLE {$this->getTable('ig_cashondelivery_foreign')} ADD COLUMN `fee_mode` enum('percent','absolute') NOT NULL default 'absolute'");
-$installer->endSetup();
-?>
