71443a1999ec89766dfa9d9b8665ff873aaf926c
Velan Dev <vispl.dev@gmail.com>
2012-06-02 13:09:55 +0530

    task:8 show the sales difference in homepage top for men and women

--- app/code/core/Mage/Customer/controllers/AccountController.php
+++ app/code/core/Mage/Customer/controllers/AccountController.php
@@ -168,9 +168,7 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Front_Action
                 $session->addError($this->__('Login and password are required.'));
             }
         }
-		
         $this->_loginPostRedirect(); 
-		
     }
 
     /**
