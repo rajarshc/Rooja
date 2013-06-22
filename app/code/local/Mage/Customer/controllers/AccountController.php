24967d4637dfe11aa8524aab43756949be4e5a6d
Saurabh <agarwalsaurabh12@gmail.com>
2013-06-22 13:24:30 +0530

    merge commit

--- app/code/local/Mage/Customer/controllers/AccountController.php
+++ app/code/local/Mage/Customer/controllers/AccountController.php
@@ -68,7 +68,7 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Front_Action
 		$action = $this->getRequest()->getActionName();
 		
 		
-		$pattern = '/^(create|login|logoutSuccess|forgotpassword|forgotpasswordpost|confirm|confirmation|ajaxLogin|ajaxForgotPassword|ajaxCheckEmail|ajaxCreate)/i';
+		$pattern = '/^(create|login|logoutSuccess|forgotpassword|forgotpasswordpost|confirm|confirmation|ajaxLogin|ajaxForgotPassword|ajaxCheckEmail|ajaxCreate|ajaxCreateOnBoard)/i';
 		if (!preg_match($pattern, $action)) {
 			if (!$this->_getSession()->authenticate($this)) {
 				$this->setFlag('', 'no-dispatch', true);
@@ -551,6 +551,130 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Front_Action
 		$this->getResponse()->setBody(Zend_Json::encode($result));
 	}
 	
+
+public function ajaxCreateOnBoardAction()
+	{
+		$session = $this->_getSession();
+		if ($session->isLoggedIn()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+		$session->setEscapeMessages(true); // prevent XSS injection in user input
+		if ($this->getRequest()->isPost()) {
+			$errors = array();
+			
+			if (!$customer = Mage::registry('current_customer')) {
+				$customer = Mage::getModel('customer/customer')->setId(null);
+			}
+			
+			/* @var $customerForm Mage_Customer_Model_Form */
+			$customerForm = Mage::getModel('customer/form');
+			$customerForm->setFormCode('customer_account_create')
+				->setEntity($customer);
+			
+			$customerData = $customerForm->extractData($this->getRequest());
+			
+			if ($this->getRequest()->getParam('is_subscribed', false)) {
+				$customer->setIsSubscribed(1);
+			}
+			
+			/**
+			 * Initialize customer group id
+			 */
+			$customer->getGroupId();
+			
+			if ($this->getRequest()->getPost('create_address')) {
+				/* @var $address Mage_Customer_Model_Address */
+				$address = Mage::getModel('customer/address');
+				/* @var $addressForm Mage_Customer_Model_Form */
+				$addressForm = Mage::getModel('customer/form');
+				$addressForm->setFormCode('customer_register_address')
+					->setEntity($address);
+				
+				$addressData    = $addressForm->extractData($this->getRequest(), 'address', false);
+				$addressErrors  = $addressForm->validateData($addressData);
+				if ($addressErrors === true) {
+					$address->setId(null)
+						->setIsDefaultBilling($this->getRequest()->getParam('default_billing', false))
+						->setIsDefaultShipping($this->getRequest()->getParam('default_shipping', false));
+					$addressForm->compactData($addressData);
+					$customer->addAddress($address);
+					
+					$addressErrors = $address->validate();
+					if (is_array($addressErrors)) {
+						$errors = array_merge($errors, $addressErrors);
+					}
+				} else {
+					$errors = array_merge($errors, $addressErrors);
+				}
+			}
+			
+			try {
+				$customerErrors = $customerForm->validateData($customerData);
+				if ($customerErrors !== true) {
+					$errors = array_merge($customerErrors, $errors);
+				} else {
+					$customerForm->compactData($customerData);
+					$customer->setPassword($this->getRequest()->getPost('password'));
+					$customer->setConfirmation($this->getRequest()->getPost('confirmation'));
+					$customerErrors = $customer->validate();
+					if (is_array($customerErrors)) {
+						$errors = array_merge($customerErrors, $errors);
+					}
+				}
+				
+				$validationResult = count($errors) == 0;
+				$result = array();
+				if (true === $validationResult) {
+					$customer->save();
+					
+					if ($customer->isConfirmationRequired()) {
+						$customer->sendNewAccountEmail('confirmation', $session->getBeforeAuthUrl());
+						$result['success'] = true;
+						$result['message'] = $this->__('Account confirmation is required. Please, check your email for the confirmation link. To resend the confirmation email please <a href="%s">click here</a>.', Mage::helper('customer')->getEmailConfirmationUrl($customer->getEmail()));
+					} else {
+						$session->setCustomerAsLoggedIn($customer);
+						$url = $this->_welcomeCustomer($customer);
+						//$this->_redirectSuccess($url);
+						//return;
+						$result['success'] = true;
+						$result['message'] = $this->__('You are successfully registered');
+					}
+				} else {
+					$session->setCustomerFormData($this->getRequest()->getPost());
+					if (is_array($errors)) {
+						$result['success'] = false;
+						foreach ($errors as $errorMessage) {
+							//$session->addError($errorMessage);
+							$result['message'] .= $errorMessage;
+						}
+					} else {
+						//$session->addError($this->__('Invalid customer data'));
+						$result['success'] = false;
+						$result['message'] = $this->__('Invalid customer data');
+					}
+				}
+			} catch (Mage_Core_Exception $e) {
+				$session->setCustomerFormData($this->getRequest()->getPost());
+				if ($e->getCode() === Mage_Customer_Model_Customer::EXCEPTION_EMAIL_EXISTS) {
+					$url = Mage::getUrl('customer/account/forgotpassword');
+					$result['success'] = false;
+					$result['message'] = $this->__('There is already an account with this email address. If you are sure that it is your email address.');
+					
+				} else {
+					$result['success'] = false;
+					$result['message'] = $e->getMessage();
+					
+				}
+				
+			} catch (Exception $e) {
+				$result['success'] = false;
+				$result['message'] = $this->__('Cannot save the customer.');
+			}
+		}
+		$this->getResponse()->setBody(Zend_Json::encode($result));
+	}
+
 	/**
 	 * Add welcome message and send new account email.
 	 * Returns success URL
