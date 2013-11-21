b3ca15840d2255b45223dadf7253a97adb3c6f13
Ankur Goyal Auriga <ankur.goyal@aurigait.com>
2013-11-21 11:21:05 +0530

    testing

--- cron.php
+++ cron.php
@@ -23,7 +23,7 @@
  * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
+//echo "ankur";
 require 'app/Mage.php';
 
 if (!Mage::isInstalled()) {
